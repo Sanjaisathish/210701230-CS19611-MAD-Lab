3d6a64c0346bfb8831f85f4bb72f34f3c26cbef1
Sanjaisathish <103747125+Sanjaisathish@users.noreply.github.com>
2024-05-17 21:33:15 +0530

    Create exp-1.kt

---
+++ exp-1.kt
@@ -0,0 +1,62 @@
+activity_main.xml
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
+
+    <TextView
+        android:id="@+id/textView"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Hello, World!"
+        android:textSize="24sp"
+        android:layout_centerInParent="true"/>
+
+    <Button
+        android:id="@+id/button"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Change Font and Color"
+        android:layout_below="@id/textView"
+        android:layout_centerHorizontal="true"
+        android:layout_marginTop="20dp"/>
+</RelativeLayout>
+
+  MainActivity.kt
+
+package com.example.textstyler
+
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            // Change the font and color of the text
+            textView.setTypeface(textView.typeface, Typeface.BOLD_ITALIC)
+            textView.setTextColor(Color.RED)
+
+            // Display a toast message
+            Toast.makeText(this, "Font and color changed!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+
